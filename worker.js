ccc0c960a29fd3b14dd1d2077aa1429f9e67fe8f
shiftsometimber <shiftsometimber@gmail.com>
2026-08-09 17:00:58 +0100

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const APP_NAME = 'Shift Core';
-const API_VERSION = '3.2C-registration-hotfix';
+const API_VERSION = '3.2D-pbkdf2-hotfix';
 const DEFAULT_ALLOWED_ORIGINS = [
   'https://shiftsometimber.co.uk',
   'https://www.shiftsometimber.co.uk',
@@ -619,7 +619,7 @@ function sessionCookie(token, expires) {
 function clearSessionCookie() { return 'sst_session=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax'; }
 
 async function hashPassword(password) {
-  const iterations = 120000;
+  const iterations = 100000;
   const salt = crypto.getRandomValues(new Uint8Array(16));
   const key = await crypto.subtle.importKey('raw', new TextEncoder().encode(password), 'PBKDF2', false, ['deriveBits']);
   const bits = await crypto.subtle.deriveBits({name:'PBKDF2',hash:'SHA-256',salt,iterations}, key, 256);
